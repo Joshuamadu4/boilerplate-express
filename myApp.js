f21e2c8afaf7020056528beabb24238193a3e85a
Joshuamadu4 <joshuamadu4@gmail.com>
2023-09-06 21:57:35 +0000

    summarry

--- myApp.js
+++ myApp.js
@@ -1,19 +1,19 @@
 let express = require('express');
 let app = express();
-absolutepath = __dirname +'/views/index.html'
+absolutepath = __dirname + '/views/index.html'
 app.use('/public', express.static(__dirname + '/public'))
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(absolutepath);
 })
 
-app.get("/json", function(req, res){
+app.get("/json", function(req, res) {
   res.json({
     message: "Hello json"
   });
 });
-app.use((req,res,next) => {
-  let {method, path, ip} = req;
-    console.log(method +" "+path+"-"+ip)
+app.use((req, res, next) => {
+  let { method, path, ip } = req;
+  console.log(method + " " + path + "-" + ip);
   next();
 })
 console.log("Hello World")
@@ -52,4 +52,4 @@ console.log("Hello World")
 
 
 
- module.exports = app;
+module.exports = app;
