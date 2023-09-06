521c0f54b3309b27781a8daf374b84f88920fbc1
Joshuamadu4 <joshuamadu4@gmail.com>
2023-09-06 00:46:58 +0000

    Using app.use

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 absolutepath = __dirname +'/views/index.html'
+app.use('/public', express.static(__dirname + '/public'))
 app.get('/', function(req, res){
   res.sendFile(absolutepath);
 })
